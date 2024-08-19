11c19d8fa03c4d610f780e02b8d20b53f6df909c
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2024-08-19 17:16:17 +0900

    [Platinum V] Title: Random Number Generator, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/1160. Random Number Generator/Random Number Generator.py
+++ 백준/Platinum/1160. Random Number Generator/Random Number Generator.py
@@ -1,14 +1,7 @@
-def solve(n):
-    if n == 1:
-        return 1
-    elif n%2 == 0:
-        return (solve(n//2)*(1 + pow(a, n//2, m)))%m
-    else:
-        return (solve(n-1) + pow(a, n-1, m))%m
-
-m,a,c,X0,n,g = map(int, input().split())
-
-if a == 1:
-    print(int(((X0 + c*n)%m)%g))
-else:
-    print(((X0*pow(a, n, m) + c*solve(n))%m)%g)
+p = pow
+def s(n):
+    if n==1:return 1
+    elif n%2==0:return(s(n//2)*(1+p(a,n//2,m)))%m
+    else:return(s(n-1)+p(a,n-1,m))%m
+m,a,c,X,n,g=map(int,input().split())
+print((X*p(a,n,m)+c*s(n))%m%g)
