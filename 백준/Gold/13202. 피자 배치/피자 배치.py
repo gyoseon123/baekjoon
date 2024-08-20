24962815cf739c5c5ef7caa2e8613b8812849c6d
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2024-08-20 12:17:04 +0900

    [Gold II] Title: 피자 배치, Time: 388 ms, Memory: 112292 KB -BaekjoonHub

--- 백준/Gold/13202. 피자 배치/피자 배치.py
+++ 백준/Gold/13202. 피자 배치/피자 배치.py
@@ -10,64 +10,26 @@ for _ in range(t):
     c = (a*a + b*b)**0.5
     r = (a + b - c)/2
     lines = [r]
-    target = (((a - r)**2 + r**2)**0.5 - r)/(2*r)
 
-    left = 0
-    right = 1
-    while True:
-        mid = (left+right)/2
-        if abs((mid / (1 - mid)) - target) < 1e-8:
-            break
-
-        if (mid / (1 - mid)) < target:
-            left = mid
-        else:
-            right = mid
+    for dir in ((a-r)**2 + r**2, r**2 + r**2, (b-r)**2 + r**2):
+        target = (dir**0.5 - r)/(2*r)
     
-    exp = 1
-    for i in range(100):
-        lines.append(r * mid**exp)
-        exp += 1
+        left = 0
+        right = 1
+        while True:
+            mid = (left+right)/2
+            if abs((mid / (1 - mid)) - target) < 1e-8:
+                break
     
-    
-    target = ((r**2 + r**2)**0.5 - r)/(2*r)
-
-    left = 0
-    right = 1
-    while True:
-        mid = (left+right)/2
-        if abs((mid / (1 - mid)) - target) < 1e-8:
-            break
-
-        if (mid / (1 - mid)) < target:
-            left = mid
-        else:
-            right = mid
-    
-    exp = 1
-    for i in range(100):
-        lines.append(r * mid**exp)
-        exp += 1
-    
-    
-    target = (((b - r)**2 + r**2)**0.5 - r)/(2*r)
-
-    left = 0
-    right = 1
-    while True:
-        mid = (left+right)/2
-        if abs((mid / (1 - mid)) - target) < 1e-8:
-            break
-
-        if (mid / (1 - mid)) < target:
-            left = mid
-        else:
-            right = mid
-    
-    exp = 1
-    for i in range(100):
-        lines.append(r * mid**exp)
-        exp += 1
+            if (mid / (1 - mid)) < target:
+                left = mid
+            else:
+                right = mid
+        
+        exp = 1
+        for i in range(100):
+            lines.append(r * mid**exp)
+            exp += 1
     
     lines.sort(reverse=True)
     print(f"{lines[k-1]**2*PI:.8f}")
