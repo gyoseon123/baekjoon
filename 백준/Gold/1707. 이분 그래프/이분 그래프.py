b2a87b8a0b1021bfa189d80ea5b5a80666bdea62
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2024-08-22 10:20:30 +0900

    [Gold IV] Title: 이분 그래프, Time: 1120 ms, Memory: 53480 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(int(1e9))
+
+def dfs(now, color):
+    ret = True
+    visited[now] = color
+    for next in graph[now]:
+        if visited[next] == -1:
+            ret = ret & dfs(next, (color+1)%2)
+        else:
+            if visited[next] == visited[now]:
+                return False
+    
+    return ret
+
+t = int(input())
+
+for _ in range(t):
+    v,e = map(int, input().split())
+    graph = [[] for _ in range(v+1)]
+    visited = [-1]*(v+1)
+    
+    for _ in range(e):
+        a,b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+
+    res = []
+    for i in range(1, v+1):
+        if visited[i] == -1:
+            res.append(dfs(i, 0))
+
+    print("YES" if all(res) else "NO")
