0b28b2b56b52f2a806922807ac53dbfdbcf6d70c
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2024-08-26 16:51:11 +0900

    [Silver I] Title: 카탈란 마스터의 선분 그리기 게임, Time: 152 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/27296. 카탈란 마스터의 선분 그리기 게임/카탈란 마스터의 선분 그리기 게임.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    if n == 0:
+        print(1, 0)
+    elif n == 1:
+        print(1, 0)
+    elif n == 2:
+        print(0, 1)
+    else:
+        print(0, 1)
