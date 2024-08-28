54f2124d08752b60f1983635a7c840dc1ef0304a
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2024-08-28 15:12:36 +0900

    [Gold III] Title: 골목 대장 호석 - 효율성 1, Time: 2420 ms, Memory: 194020 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.py
+++ 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.py
@@ -3,30 +3,25 @@ import sys
 input = sys.stdin.readline
 INF = sys.maxsize
 
-def dijkstra(start):
+def dijkstra(start, target):
     q = []
     distance = [INF]*(n+1)
     
-    heapq.heappush(q, (0, 0, start))
+    heapq.heappush(q, (0, start))
     distance[start] = 0
     
-    ret = INF
-    
     while q:
-        dis, now_max, now = heapq.heappop(q)
-            
-        if dis > c: continue
-        
-        if now == b:
-            ret = min(ret, now_max)
+        dis, now = heapq.heappop(q)
         
         for next, cst in graph[now]:
+            if cst > target: continue
+            
             cost = dis + cst
             if cost < distance[next]:
                 distance[next] = cost
-                heapq.heappush(q, (cost, max(now_max, cst), next))
-                
-    return ret
+                heapq.heappush(q, (cost,  next))
+            
+    return distance[b]
 
 n,m,a,b,c = map(int, input().split())
 graph = [[] for _ in range(n+1)]
@@ -36,6 +31,15 @@ for _ in range(m):
     graph[u].append((v, cst))
     graph[v].append((u, cst))
 
+left = 0
+right = 21
 
-d = dijkstra(a)
-print(d if d != INF else -1)
+while left + 1 < right:
+    mid = (left + right)//2
+    
+    if dijkstra(a, mid) <= c:
+        right = mid
+    else:
+        left = mid
+
+print(right if right != 21 else -1)
