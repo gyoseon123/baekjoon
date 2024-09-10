b185e429f23f3d748803bb592d9fb28827f4e61d
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2024-09-10 14:16:07 +0900

    [Gold III] Title: 격자 게임, Time: 372 ms, Memory: 115932 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28218. 격자 게임/격자 게임.py
+++ 백준/Gold/28218. 격자 게임/격자 게임.py
@@ -8,14 +8,21 @@ dp[n][m] = 0
 
 for i in range(n, 0, -1):
     for j in range(m, 0, -1):
-        if dp[i][j] != -1:
-            if i - 1 >= 0 and dp[i-1][j] == -1 and board[i-2][j-1] != "#":
-                dp[i-1][j] = dp[i][j]^1
-            if j - 1 >= 0 and dp[i][j-1] == -1 and board[i-1][j-2] != "#":
-                dp[i][j-1] = dp[i][j]^1
+        if dp[i][j] == -1 and board[i-1][j-1] != "#":
+            arr = []
+            if i + 1 <= n and dp[i+1][j] != -1:
+                arr.append(dp[i+1][j])
+            if j + 1 <= m and dp[i][j+1] != -1:
+                arr.append(dp[i][j+1])
+            
             for kk in range(1, k+1):
-                if 0 <= i - kk < n and 0 <= j - kk < n and dp[i-kk][j-kk] == -1 and board[i-kk-1][j-kk-1] != "#":
-                    dp[i-kk][j-kk] = dp[i][j]^1
+                if 0 <= i + kk <= n and 0 <= j + kk <= m and dp[i+kk][j+kk] != -1:
+                    arr.append(dp[i+kk][j+kk])
+            
+            if all(arr):
+                dp[i][j] = 0
+            else:
+                dp[i][j] = 1
 
 q = int(input())
 for _ in range(q):
