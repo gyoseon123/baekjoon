f4bd345745e612f2887867726fe70f2b42af0d46
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2024-09-23 12:52:05 +0900

    [Gold IV] Title: Road To Savings, Time: 108 ms, Memory: 111128 KB -BaekjoonHub

--- 백준/Gold/27617. Road To Savings/Road To Savings.py
+++ 백준/Gold/27617. Road To Savings/Road To Savings.py
@@ -1,67 +1,46 @@
 import heapq
 import sys
 input = sys.stdin.readline
-INF = int(1e9)
+INF = sys.maxsize
 
 def dijkstra(start):
-    trac = [[] for _ in range(n+1)]
-    dist = [INF]*(n+1)
-    dist[start] = 0
+    distance = [INF]*(n+1)
+    distance[start] = 0
     q = []
     heapq.heappush(q, (0, start))
-
-    while q:
-        c, now  = heapq.heappop(q)
-        if c > dist[now]: continue
-
-        for next, cst in graph[now]:
-            cost = cst + c
-            if cost < dist[next]:
-                dist[next] = cost
-                trac[next].append((cost, now))
-                heapq.heappush(q, (cost, next))
-            if cost == dist[next]:
-                trac[next].append((cost, now))
     
-    rett = [[] for _ in range(n+1)]
-    for i in range(n+1):
-        if not trac[i]: continue
-
-        min_cst = min([j[0] for j in trac[i]])
-        for cst, node in trac[i]:
-            if cst == min_cst: rett[i].append(node)
-
-    return (rett, dist[d])
-
-def dfs(now):
-    visited[now] = True
-    for next in trac[now]:
-        for edge in graph[next]:
-            if edge[0] == now:
-                graph[next].remove(edge)
-                graph[now].remove([next, edge[1]])
-                break
-        if not visited[next]:
-            dfs(next)
+    while q:
+        dis, now = heapq.heappop(q)
+        
+        if dis > distance[now]: continue
+        
+        for next, cst in graph[now]:
+            cost = cst + distance[now]
+            if cost < distance[next]:
+                distance[next] = cost
+                heapq.heappush(q, (cost, next))
+    
+    return distance
 
 
-n,m,s,d = map(int, input().split())
-
-
+n,m,a,b = map(int, input().split())
 graph = [[] for _ in range(n+1)]
+edges = []
 
 for _ in range(m):
-    u,v,p = map(int, input().split())
-    graph[u].append([v,p])
-    graph[v].append([u,p])
-
-fir_d = dijkstra(s)
-trac = fir_d[0]
-visited = [False]*(n+1)
-dfs(d)
+    u,v,c = map(int, input().split())
+    graph[u].append((v,c))
+    graph[v].append((u,c))
+    edges.append((u,v,c))
+    
+d1 = dijkstra(a)
+d2 = dijkstra(b)
 
 ans = 0
-for l in graph:
-    if l: ans += sum([i[1] for i in l])
+min_c = d1[b]
 
-print(ans//2)
+for u,v,c in edges:
+    if d1[v] + d2[u] + c == min_c or d1[u] + d2[v] + c == min_c: continue
+    ans += c
+
+print(ans)
