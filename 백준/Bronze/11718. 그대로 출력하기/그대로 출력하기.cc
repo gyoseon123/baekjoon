90417f51bb3c554dd0bc5310a8faca8018b65476
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2024-12-21 00:43:43 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <unordered_set>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s;
+
+    while (getline(cin, s)){
+        cout << s << '\n';
+    }
+
+    return 0;
+}
