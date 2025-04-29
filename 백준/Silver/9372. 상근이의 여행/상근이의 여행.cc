fef898a1261f3f225c651075c068692e5d5da443
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2025-04-29 19:47:55 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t; cin >> t;
+    while (t--){
+        int n,m; cin >> n >> m;
+        for (int i = 0; i < m; i++){
+            int u,v; cin >> u >> v;
+        }
+        cout << n-1 << '\n';
+    }
+
+
+    return 0;
+}
