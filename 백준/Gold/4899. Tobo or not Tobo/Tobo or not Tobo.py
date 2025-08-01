8b64d5bffb3ed93c9f2a9ead4a6960aca92fcc4e
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2025-08-01 21:09:39 +0900

    [Gold III] Title: Tobo or not Tobo, Time: 816 ms, Memory: 213768 KB -BaekjoonHub

--- 백준/Gold/4899. Tobo or not Tobo/Tobo or not Tobo.py
+++ 백준/Gold/4899. Tobo or not Tobo/Tobo or not Tobo.py
@@ -1,58 +1,59 @@
+# ai test
+
 from collections import deque
-import sys
-input = sys.stdin.readline
 
-def trans(arr):
-    return "".join(arr)
+# 다이얼별 좌표 (인덱스)
+dials = [
+    [0, 1, 3, 4],  # A
+    [1, 2, 4, 5],  # B
+    [3, 4, 6, 7],  # C
+    [4, 5, 7, 8],  # D
+]
 
-def rot_r(p, board):
-    board[0+p], board[1+p], board[3+p], board[4+p] = board[3+p], board[0+p], board[4+p], board[1+p]
-    return board
-
-def rot_l(p, board):
-    board[0+p], board[1+p], board[3+p], board[4+p] = board[1+p], board[4+p], board[0+p], board[3+p]
-    return board
-
-def bfs(start):
-    if start == answer: return 0
-    ret = int(1e9)
-    q = deque()
-    q.append((start, 0))
-    visited = set()
-    visited.add(trans(start))
-    while q:
-        now, dis = q.popleft()
-
-        for i in (0,1,3,4):
-            r = rot_r(i, now[:])
-            if trans(r) not in visited:
-                if r == answer:
-                    return dis+1
-                visited.add(trans(r))
-                q.append((r[:], dis+1))
-
-            l = rot_l(i, now[:])
-            if trans(l) not in visited:
-                if l == answer:
-                    return dis+1
-                visited.add(trans(l))
-                q.append((l[:], dis+1))
-
-    return ret
-
-t = 1
-while True:
-    test = input().rstrip()
-    if test == "0000000000": break
-
-    y = int(test[0])
-    oboard = list(test[1:])
-    ans = int(1e9)
-    answer = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    b = bfs(oboard)
-
-    if b <= y:
-        print(f"{t}. {b}")
+def rotate(state, dial, clockwise=True):
+    state = list(state)
+    indices = dials[dial]
+    # 시계 방향
+    if clockwise:
+        state[indices[0]], state[indices[1]], state[indices[2]], state[indices[3]] = (
+            state[indices[2]], state[indices[0]], state[indices[3]], state[indices[1]]
+        )
     else:
-        print(f"{t}. -1")
-    t += 1
+        state[indices[0]], state[indices[1]], state[indices[2]], state[indices[3]] = (
+            state[indices[1]], state[indices[3]], state[indices[0]], state[indices[2]]
+        )
+    return ''.join(state)
+
+def solve_tobo(Y, arr):
+    goal = '123456789'
+    start = arr
+    if start == goal:
+        return 0
+    visited = set()
+    queue = deque()
+    queue.append((start, 0))  # (상태, 회전 수)
+    visited.add(start)
+    while queue:
+        cur, depth = queue.popleft()
+        if depth >= Y:  # Y 초과하면 답 X
+            continue
+        for d in range(4):  # 4개 다이얼
+            for cw in [True, False]:  # 시계/반시계
+                nxt = rotate(cur, d, cw)
+                if nxt == goal:
+                    return depth + 1
+                if nxt not in visited:
+                    visited.add(nxt)
+                    queue.append((nxt, depth + 1))
+    return -1
+
+case_num = 1
+while True:
+    line = input().strip()
+    if line == '0000000000':
+        break
+    Y = int(line[0])
+    arr = line[1:]
+    res = solve_tobo(Y, arr)
+    print(f"{case_num}. {res}")
+    case_num += 1
