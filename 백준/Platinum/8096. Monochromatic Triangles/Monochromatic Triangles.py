3aad03593ed1d57884c44e7f1de1bcd8a2e222cf
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2025-08-07 22:05:11 +0900

    [Platinum IV] Title: Monochromatic Triangles, Time: 116 ms, Memory: 110576 KB -BaekjoonHub

---
+++ 백준/Platinum/8096. Monochromatic Triangles/Monochromatic Triangles.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+indegree = [0]*(n+1)
+for _ in range(m):
+    u,v = map(int, input().split())
+    indegree[u] += 1
+    indegree[v] += 1
+
+ans = n * (n-1) * (n-2) // 6
+cnt = 0
+for i in range(1, n+1):
+    cnt += indegree[i] * (n - indegree[i] - 1)
+
+cnt //= 2
+print(ans - cnt)
