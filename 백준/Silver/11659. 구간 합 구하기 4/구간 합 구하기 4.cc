8283552d4c355d204482a814ae02f8c1f47e310b
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2025-10-04 18:04:29 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 72 ms, Memory: 5256 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,141 +1,59 @@
 #include <bits/stdc++.h>
-// #define ll long long
-#define int long long
+#define ll long long
+//#define int long long
 const int inf = 1e9;
+
 using namespace std;
 
-
 int n,m;
+int sqrtN;
 int a[101010];
+int ans[101010];
 
-struct node {
-    node *l, *r, *p;
-    int sz, mn, mx, value, sum;
-} *root;
+struct Query{
+    int s,e,idx;
 
-node *ptr[101010];
-
-void Update(node *x){
-    x->sz = 1;
-    x->sum = x->mn = x->mx = x->value;
-
-    if (x->l){
-        node *a = x->l;
-        x->sz += a->sz;
-        x->sum += a->sum;
-        x->mn = min(x->mn, a->mn);
-        x->mx = max(x->mx, a->mx);
+    bool operator < (Query &other){
+        if (s/sqrtN != other.s/sqrtN) return s/sqrtN < other.s/sqrtN;
+        else return e < other.e;
     }
-    if (x->r){
-        node *a = x->r;
-        x->sz += a->sz;
-        x->sum += a->sum;
-        x->mn = min(x->mn, a->mn);
-        x->mx = max(x->mx, a->mx);
-    }
-}
 
-void Rotate(node *x) {
-    node *p = x->p;
-    node *b;
-    if (x == p->l) {
-        p->l = b = x->r;
-        x->r = p;
-    } else {
-        p->r = b = x->l;
-        x->l = p;
-    }
-    x->p = p->p;
-    p->p = x;
-    if (b) b->p = p;
-    (x->p ? p == x->p->l ? x->p->l : x->p->r : root) = x;
-    Update(p);
-    Update(x);
-}
+    Query(int s, int e, int idx) : s(s), e(e), idx(idx) {}
+};
 
-void Splay(node *x) {
-    while (x->p) {
-        node *p = x->p;
-        node *g = p->p;
-        if (g) Rotate((x == p->l) == (p == g->l) ? p : x);
-        Rotate(x);
-    }
-}
-
-void Kth(int k){
-    node *x = root;
-    k++;
-    while (1){
-        while (x->l && x->l->sz > k) x = x->l;
-        if (x->l) k -= x->l->sz;
-        if (!k--) break;
-        x = x->r;
-    }
-    Splay(x);
-}
-
-node* Interval(int l, int r){
-    Kth(l-1);
-    node *x = root;
-    root = x->r;
-    root->p = NULL;
-    Kth(r-l);
-    x->r = root;
-    root->p = x;
-    root = x;
-    return root->r->l;
-}
-
-int Sum(int l, int r){
-    node *x = Interval(l,r);
-    return x->sum;
-}
-
-int Min(int l, int r){
-    node *x = Interval(l,r);
-    return x->mn;
-}
-
-int Max(int l, int r){
-    node *x = Interval(l,r);
-    return x->mx;
-}
-
-void Init(int n){
-    node *x = new node;
-    ptr[0] = root = x;
-    x->l = x->r = x->p = NULL;
-    x->sz = 1;
-    x->value = -inf;
-    for (int i = 1; i <= n; i++){
-        ptr[i] = x->r = new node;
-        x->r->p = x;
-        x = x->r;
-        x->l = x->r = NULL;
-        x->sz = 1;
-        x->value = a[i-1];
-    }
-    x->r = new node;
-    x->r->p = x;
-    ptr[n+1] = x = x->r;
-    x->l = x->r = NULL;
-    x->sz = 1;
-    x->value = inf;
-    for (int i = n+1; i >= 0; i--) Update(ptr[i]);
-}
-
-signed main(){
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin >> n >> m;
-    for (int i = 0; i < n; i++) cin >> a[i];
-    Init(n);
+    for (int i = 1; i <= n; i++) cin >> a[i];
 
+    sqrtN = sqrt(n);
+    vector<Query> qry;
+    
     for (int i = 0; i < m; i++){
-        int a,b; cin >> a >> b;
-        cout << Sum(a-1, b-1) << '\n';
+        int s,e; cin >> s >> e;
+        qry.push_back(Query(s, e, i));
     }
 
+    sort(qry.begin(), qry.end());
+
+    int sum = 0;
+    int s = qry[0].s;
+    int e = qry[0].e;
+    for (int i = qry[0].s; i <= qry[0].e; i++) sum += a[i];
+    ans[qry[0].idx] = sum;
+
+    for (int i = 1; i < m; i++){
+        Query now = qry[i];
+        while (s < now.s) sum -= a[s++];
+        while (s > now.s) sum += a[--s];
+        while (e < now.e) sum += a[++e];
+        while (e > now.e) sum -= a[e--];
+        ans[now.idx] = sum;
+    }
+
+    for (int i = 0; i < m; i++) cout << ans[i] << '\n';
+    
     return 0;
 }
