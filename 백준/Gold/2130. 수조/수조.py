0f913c6ec53da7ae4223fec161504aba95f443d1
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2025-10-05 18:41:11 +0900

    [Gold I] Title: 수조, Time: 208 ms, Memory: 116456 KB -BaekjoonHub

---
+++ 백준/Gold/2130. 수조/수조.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+def check(x):
+    ret = 0
+    for i in range(n):
+        b, h, w, d = tank[i]
+        if b >= x: continue
+        ret += (min(b + h, x) - b) * w * d
+    return ret >= v
+
+n = int(input())
+tank = [list(map(int, input().split())) for _ in range(n)]
+v = int(input())
+
+vol = 0
+for i in range(n):
+    vol += tank[i][1] * tank[i][2] * tank[i][3]
+    
+if vol < v:
+    print("OVERFLOW")
+    exit()
+
+left = 0
+right = 1e9
+
+for _ in range(100):
+    mid = (left+right)/2
+    
+    if check(mid): right = mid
+    else: left = mid
+
+if right > 1e7: print("OVERFLOW")
+else: print(f"{right:.2f}")
