9ac2c2eec15ab05a26bc7078d5132dc56e8a52a9
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2025-11-04 21:35:10 +0900

    [Gold I] Title: 구간 합 구하기, Time: 192 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,62 +1,57 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_set>
+#include <bits/stdc++.h>
+// #define ll long long
 #define int long long
+const int inf = 1e9;
 
 using namespace std;
 
+int tree[4040404];
+int a[1010101];
 int n,m,k;
-int arr[1000000];
-int tree[4000000];
 
 int init(int node, int start, int end){
-    if (start == end) return tree[node] = arr[start];
+    if (start == end) return tree[node] = a[start];
+
     int mid = (start+end)/2;
     return tree[node] = init(node*2, start, mid) + init(node*2+1, mid+1, end);
 }
 
-int sum_query(int node, int start, int end, int left, int right){
-    if (left <= start && end <= right) return tree[node];
-    if (right < start || end < left) return 0;
+void update(int node, int start, int end, int idx, int val){
+    if (idx < start || end < idx) return;
+    if (start == end){
+        tree[node] = a[idx] = val;
+        return;
+    }
+
     int mid = (start+end)/2;
-    return sum_query(node*2, start, mid, left, right) + sum_query(node*2+1, mid+1, end, left, right);
+    update(node*2, start, mid, idx, val);
+    update(node*2+1, mid+1, end, idx, val);
+    tree[node] = tree[node*2] + tree[node*2+1];
 }
 
-int update_query(int node, int start, int end, int idx, int diff){
-    if (start <= idx && idx <= end){
-        tree[node] += diff;
-        if (start != end){
-            int mid = (start+end)/2;
-            update_query(node*2, start, mid, idx, diff);
-            update_query(node*2+1, mid+1, end, idx, diff);
-        }
-    }
-    return 0;
+int qry(int node, int start, int end, int left, int right){
+    if (right < start || end < left) return 0;
+    if (left <= start && end <= right) return tree[node];
+
+    int mid = (start+end)/2;
+
+    return qry(node*2, start, mid, left, right) + qry(node*2+1, mid+1, end, left, right);
 }
 
 signed main(){
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
-    cout.tie(NULL);
+    cin.tie(NULL);
 
     cin >> n >> m >> k;
-    for (int i = 0; i < n; i++) cin >> arr[i];
+    for (int i = 0; i < n; i++) cin >> a[i];
     init(1, 0, n-1);
 
     for (int i = 0; i < m+k; i++){
         int a,b,c; cin >> a >> b >> c;
         if (a == 1){
-            int diff = c - arr[b-1];
-            arr[b-1] = c;
-            update_query(1, 0, n-1, b-1, diff);
+            update(1, 0, n-1, b-1, c);
         } else {
-            cout << sum_query(1, 0, n-1, b-1, c-1) << '\n';
+            cout << qry(1, 0, n-1, b-1, c-1) << '\n';
         }
     }
     
