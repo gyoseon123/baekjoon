04a7c775bbb56f55cf832f94916d7282b6b5dbc8
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2026-04-16 09:10:28 +0900

    [Platinum II] Title: 고속도로, Time: 5408 ms, Memory: 85196 KB -BaekjoonHub

---
+++ Python/백준/Platinum/10254. 고속도로/고속도로.py
@@ -0,0 +1,58 @@
+import sys
+input = sys.stdin.readline
+
+def dist_sq(x1, y1, x2, y2):
+    return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+
+def ccw(x1, y1, x2, y2, x3, y3):
+    return (x2 - x1)*(y3 - y1) - (y2 - y1)*(x3 - x1)
+
+def convex_hull(points):
+    points.sort()
+    stk1 = []
+    stk2 = []
+    for point in points:
+        while len(stk1) >= 2:
+            if ccw(*stk1[-2], *stk1[-1], *point) >= 0:
+                stk1.pop()
+            else:
+                break
+             
+        while len(stk2) >= 2:
+            if ccw(*stk2[-2], *stk2[-1], *point) <= 0:
+                stk2.pop()
+            else:
+                break 
+            
+        stk1.append(point)
+        stk2.append(point)
+    
+    return stk1 + list(reversed(stk2[1:len(stk2)-1]))
+
+def find_point(x1, y1, x2, y2, x3, y3): # ch[i+1], ch[r], ch[r+1]
+    return (x3 - x2 + x1, y3 - y2 + y1)
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    point = [tuple(map(int, input().split())) for _ in range(n)]
+    
+    ch = convex_hull(point)
+    
+    r = 0
+    ans = 0
+    m = len(ch)
+    for i in range(m):
+        while (r < 2*m and ccw(*ch[i], *ch[(i+1)%m], *find_point(*ch[(i+1)%m], *ch[r%m], *ch[(r+1)%m])) <= 0):
+            dist = dist_sq(*ch[i], *ch[r%m])
+            if dist > ans:
+                res = (i,r)
+                ans = dist
+            r += 1
+        dist = dist_sq(*ch[i], *ch[r%m])
+        if dist > ans:
+            res = (i,r)
+            ans = dist
+    
+    print(*ch[res[0]], *ch[res[1]])
